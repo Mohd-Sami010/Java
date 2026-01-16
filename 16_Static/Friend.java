3577f234669142bf3ec878f0aab941c3d9d5d4e8
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-01-16 12:32:04 +0530

    Add Friend and Testing classes to demonstrate static variable usage

---
+++ 16_Static/Friend.java
@@ -0,0 +1,9 @@
+public class Friend {
+    static int numOfFriends;
+    String name;
+
+    Friend(String name){
+        this.name = name;
+        numOfFriends++;
+    }
+}
---
+++ 16_Static/Testing.java
@@ -0,0 +1,10 @@
+public class Testing {
+    public static void main(){
+        Friend friend1 = new Friend("Spongbob");
+        Friend friend2 = new Friend("Patrick");
+        Friend friend3 = new Friend("Squid");
+        Friend friend4 = new Friend("Sandy");
+
+        System.out.println(Friend.numOfFriends);
+    }
+}
